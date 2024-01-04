ed15b0e078d0f38a0d1e23aa991ca41dd5858fbb
Samuel Steele <steelesam72@gmail.com>
2024-01-04 05:14:37 -0500

    - add code that reference LethalLib in the sample plugin

--- test/SamplePlugin.cs
+++ test/SamplePlugin.cs
@@ -5,6 +5,7 @@ public sealed class SamplePlugin : BaseUnityPlugin
 {
     public void Awake()
     {
+        Logger.LogInfo($"TEST: {LethalLib.Modules.Levels.LevelTypes.OffenseLevel}");
         _ = Harmony.CreateAndPatchAll(
             typeof(SamplePlugin).Assembly,
             GeneratedPluginInfo.Identifier);
